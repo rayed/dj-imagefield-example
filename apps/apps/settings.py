2acf4e293eb08450e113da14dac94a5b6b8d76f6
Rayed Alrashed <rayed@rayed.com>
2021-09-23 11:25:32 +0300

    Upgrade to latest LTS 3.2

--- apps/apps/settings.py
+++ apps/apps/settings.py
@@ -124,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, '..','www','static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..','www','media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
